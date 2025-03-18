b3c150607c5d507bec9b4389d5e77ba83d67b756
Isabellaaramos <Isabellaramos7@outlook.com>
2025-03-18 16:55:29 -0300

    adiciona configuração do Tailwind CSS e refatora componente

--- src/pages/Hero.jsx
+++ src/pages/Hero.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import{ motion } from 'framer-motion';
-import profilepic from '../images/profilepic.jpg';
+
 
 
 
@@ -9,17 +9,18 @@ export const Hero = () => {
         <div className='relative overflow-clip min-h-screen text-white bg-[liner-gradient(to_bottom,#000,#071E18_35%,#208A65_67%,#35FB8E_85%)]'>
 
             <div
-              className="absolute bg-black w-[2400px] h[1000px] rounded-[50%] left-1/2 -translate-x1/2
+              className="absolute  bg-black w-[2400px] h[1000px] rounded-[50%] left-1/2 -translate-x1/2
                 bg-[radial-gradient(closest-side,#000_85%,#249974)] top-[450px] 
                 border-[1px] border-[#8CD6DE]/30"
             
             
             />
 
-
-            
+            <div>
+               
+            </div>
 
             
         </div>
-    )
-}
+    );
+};
