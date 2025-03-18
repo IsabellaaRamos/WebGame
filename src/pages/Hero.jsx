c98aa7e38f3d5d8dc51e915b2453f60a91aa9f66
Isabellaaramos <Isabellaramos7@outlook.com>
2025-03-18 10:28:46 -0300

    adiciona elemento de fundo ao componente Hero

--- src/pages/Hero.jsx
+++ src/pages/Hero.jsx
@@ -8,6 +8,17 @@ export const Hero = () => {
     return (
         <div className='relative overflow-clip min-h-screen text-white bg-[liner-gradient(to_bottom,#000,#071E18_35%,#208A65_67%,#35FB8E_85%)]'>
 
+            <div
+              className="absolute bg-black w-[2400px] h[1000px] rounded-[50%] left-1/2 -translate-x1/2
+                bg-[radial-gradient(closest-side,#000_85%,#249974)] top-[450px] 
+                border-[1px] border-[#8CD6DE]/30"
+            
+            
+            />
+
+
+            
+
             
         </div>
     )
